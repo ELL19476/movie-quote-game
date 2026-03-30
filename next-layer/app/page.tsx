c9e85285dacef51f1263ff0d17f4b84e7fb9017d
Felix Kaspar <ich@felixkaspar.com>
2026-03-30 03:03:08 +0200

    server side randomness

--- next-layer/app/page.tsx
+++ next-layer/app/page.tsx
@@ -1,6 +1,9 @@
 import QuoteClient from "./QuoteClient";
 import { getRandomQuoteFromTimestamps } from "@/lib/stamps";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function Home() {
     const quote = getRandomQuoteFromTimestamps();
 
